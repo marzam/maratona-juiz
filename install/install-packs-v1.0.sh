812555452229f7d276ea5669aa0368847fc8a4f5
marzam <zamith.marcelo@gmail.com>
2020-10-26 00:08:13 +0000

    2020-10-25 release #1

---
+++ install/install-packs-v1.0.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+echo 'Instalando pacotes para o sistema da maratona'
+apt-get install build-essential gcc g++ rsync -y
+apt-get install git -y
+apt-get install python3-dev default-libmysqlclient-dev  -y
+apt-get install apache2 -y
+apt-get install mysql-server -y
+apt-get install php libapache2-mod-php php-mysql -y
+mysql_secure_installation
---
+++ install/sync.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+rsync -av --progress /home/localuser/sistema/maratona-juiz/html/  /var/www/html/
