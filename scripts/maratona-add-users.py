352afab1e4bc74f84be7efc56c5cdb0be375328f
marzam <zamith.marcelo@gmail.com>
2020-10-30 20:44:08 -0300

    2020-10-31 release #7

--- scripts/maratona-add-users.py
+++ scripts/maratona-add-users.py
@@ -11,8 +11,8 @@ def getNewPassword():
     p =  "".join(random.sample(s,passlen ))
     return p
 
-def sendResetPassword(username, to_email):
-    userPasswd = getNewPassword()
+def sendResetPassword(username, to_email, userPasswd):
+    
     userPasswd_MD5 = hashlib.md5(str(userPasswd).encode('utf-8')).hexdigest()
     
     
@@ -56,7 +56,8 @@ if __name__ == "__main__":
                     sys.exit(-1)
         #m_facc = r['faccess']
         #sql = 'INSERT INTO login (name, username, password, type, email, actived) values ("{0:s}", "{1:s}", "{2:s}",  "{3:d}", "{4:s}","1");'.format(r['name'], r['username'],  r['email'],  i_type,  hashlib.md5(r['password']).hexdigest())
-        sql = 'INSERT INTO login (name, username, email , type, password, actived, fasscess) values ("{0}", "{1}", "{2}", "{3}", "{4}", "1","{5}");'.format(r['name'], r['username'],  r['email'],  i_type,  hashlib.md5(str(r['password']).encode('utf-8')).hexdigest(), r['faccess'])
+        passwd = getNewPassword
+        sql = 'INSERT INTO login (name, username, email , type, password, actived, fasscess) values ("{0}", "{1}", "{2}", "{3}", "{4}", "1","{5}");'.format(r['name'], r['username'],  r['email'],  i_type,  hashlib.md5(passwd.encode('utf-8')).hexdigest(), r['faccess'])
         #sql = 'INSERT INTO login (name, username, email , type, password, actived, fasscess) values ("{0}", "{1}", "{2}", "{3}", "{4}", "1","{5}");'.format(r['name'], r['username'],  r['email'],  i_type,  r['password'], r['faccess'])
         #print(sql)
         cursor.execute(sql)
